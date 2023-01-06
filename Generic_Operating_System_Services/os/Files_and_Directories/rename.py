2cb52a3249348901d92f3ea74217eb1190f05a09
karpovvadim <karpovvadim@mail.ru>
2023-01-06 12:04:29 +0300

    countinue_3

--- Generic_Operating_System_Services/os/Files_and_Directories/rename.py
+++ Generic_Operating_System_Services/os/Files_and_Directories/rename.py
@@ -19,5 +19,22 @@
 #      Если вы хотите кросс-платформенную перезапись адресата, используйте replace().
 #      Вызывает событие аудита os.rename с аргументами src, dst, src_dir_fd, dst_dir_fd.
 #      Новое в версии 3.3: Аргументы src_dir_fd и dst_dir_fd.
-#      Изменено в версии 3.6: принимает объект, подобный пути, для src и dst.
+#      Изменено в версии 3.6: принимает объект  path-like object (подобный пути), для src и dst.
 import os
+scr_dir, dst_dir = 'test_dir', 'rename_dir'
+os.mkdir(scr_dir, 0o774)
+print('scr_dir:', os.path.isdir(scr_dir), '  dst_dir:', os.path.isdir(dst_dir))
+os.rename(scr_dir, dst_dir)
+print('scr_dir:', os.path.isdir(scr_dir), ' dst_dir:', os.path.isdir(dst_dir))
+os.rmdir(dst_dir)
+print('scr_dir:', os.path.isdir(scr_dir), ' dst_dir:', os.path.isdir(dst_dir))
+print('-----------------------------------------------------')
+scr_f, dst_f = 'test_file.txt', 'rename_file.txt'
+fp = open(scr_f, 'w')
+print('scr_f:', os.path.isfile(scr_f), '  dst_f:', os.path.isfile(dst_f))
+fp.write('data string')
+fp.close()
+os.rename(scr_f, dst_f)
+print('scr_f:', os.path.isfile(scr_f), '  dst_f:', os.path.isfile(dst_f))
+os.unlink(dst_f)
+print('scr_f:', os.path.isfile(scr_f), '  dst_f:', os.path.isfile(dst_f))
